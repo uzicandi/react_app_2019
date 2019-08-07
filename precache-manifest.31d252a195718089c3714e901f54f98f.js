d4bd48c89daaa31aeec7a3d329d7105b651a8d1b
uzicandi <kys7196@gmail.com>
2019-08-07 18:33:24 +0900

    Updates

---
+++ precache-manifest.31d252a195718089c3714e901f54f98f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec82dc4b2a7aa574c1c413c03c1280a5",
+    "url": "/react_app_2019/index.html"
+  },
+  {
+    "revision": "dc69c7335356c8a01840",
+    "url": "/react_app_2019/static/css/main.c01ece02.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/react_app_2019/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "dc69c7335356c8a01840",
+    "url": "/react_app_2019/static/js/main.a9d31950.chunk.js"
+  },
+  {
+    "revision": "c603e03a62d8f9904a24",
+    "url": "/react_app_2019/static/js/runtime~main.a461a44b.js"
+  }
+]);
